e380bfb314ad309b2b7348dce3795d32a0aa09b0
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-20 09:51:32 +0530

    Create CO23AD0701_2_AA1.py

---
+++ CO23AD0701_2_AA1.py
@@ -0,0 +1,8 @@
+myFavoriteMovie = "Lion King"
+print(myFavoriteMovie)
+voiceActor = "James Earl"
+voiceActress = "Beyonce"
+print(voiceActor)
+print(voiceActress)
+yearOfRelease = 1994
+print(yearOfRelease)
